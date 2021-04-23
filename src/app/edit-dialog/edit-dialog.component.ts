758170dbf256064f051705dfd70869634907d75b
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-23 15:07:39 +0530

    dialog box for editing the movie added

---
+++ src/app/edit-dialog/edit-dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
+import { ApiServiceService } from "../api-service.service";
+
+@Component({
+  selector: 'app-edit-dialog',
+  templateUrl: './edit-dialog.component.html',
+  styleUrls: ['./edit-dialog.component.css']
+})
+export class EditDialogComponent implements OnInit {
+
+  constructor(private api: ApiServiceService, private route: ActivatedRoute,  private router: Router) { }
+
+  ngOnInit(): void {}
+  sendData(data: any){
+    console.log(data)
+
+    const path:any = this.route.snapshot.paramMap;
+    console.log(path)
+    const movie = {
+      id: path.params.movie || null,
+      name: data.movie  || null,
+      plot: data.plot  || null,
+      poster: data.poster  || null,
+      actors: data.actors.split(',')[0]!==''? data.actors.split(',') : null 
+    }
+    this.api.editMovie(movie).subscribe(data=>{
+      this.router.navigate([".."]);
+    })
+  }
+  cancel(){
+    this.router.navigate([".."]);
+  }
+}
