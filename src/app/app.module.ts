7758ec03b04aecdedef0667c3f20f6d755898a6e
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-23 15:12:51 +0530

    angular material dialog module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { ProducersComponent } from './producers/producers.component';
+import { ActorsComponent } from './actors/actors.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from "@angular/material/dialog";
+import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
+import { NewActorComponent } from './new-actor/new-actor.component';
+import { NewMovieComponent } from './new-movie/new-movie.component';
+import { NewProducerComponent } from './new-producer/new-producer.component';
 
+const material = [
+  MatDialogModule
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MoviesComponent,
+    ProducersComponent,
+    ActorsComponent,
+    HomepageComponent,
+    PageNotFoundComponent,
+    EditDialogComponent,
+    NewActorComponent,
+    NewMovieComponent,
+    NewProducerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    material
+  ],
+  entryComponents:[
+    EditDialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
