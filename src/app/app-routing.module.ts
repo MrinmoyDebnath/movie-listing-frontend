207f1947bb3050beb79bfae61df3a5aa36218f02
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-23 15:10:19 +0530

    routes added for movies, producers, actors and search queries and params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,64 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActorsComponent } from './actors/actors.component';
+import { MoviesComponent } from './movies/movies.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProducersComponent } from "./producers/producers.component";
+import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
+import { NewActorComponent } from './new-actor/new-actor.component';
+import { NewMovieComponent } from './new-movie/new-movie.component';
+import { NewProducerComponent } from './new-producer/new-producer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+  },
+  {
+    path: 'movies/:id',
+    component: MoviesComponent
+  },
+  {
+    path: 'movies/movie/:id',
+    component: MoviesComponent
+  },
+  {
+    path: 'actors',
+    component: ActorsComponent
+  },
+  {
+    path: 'actors/:name',
+    component: ActorsComponent
+  },
+  {
+    path: 'producers/:name',
+    component: ProducersComponent
+  },
+  {
+    path: 'producers',
+    component: ProducersComponent
+  },
+  {
+    path: 'edit/:movie',
+    component: EditDialogComponent
+  },
+  {
+    path: 'new-actor',
+    component: NewActorComponent
+  },
+  {
+    path: 'new-movie',
+    component: NewMovieComponent
+  },
+  {
+    path: 'new-producer',
+    component: NewProducerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
