9cd14798a93b9694be03d6c9e8346f65e6aa1ace
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-27 16:16:30 +0530

    added interceptor for token auth header

---
+++ src/app/token-interceptor.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Injector } from '@angular/core';
+import { HttpHandler, HttpInterceptor } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor{
+
+  constructor(private injector: Injector) { }
+  getToken(){
+    return localStorage.getItem('token');
+  }
+  intercept(req: any, next: HttpHandler){
+    const token_request = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.getToken()}`
+      }
+    })
+    return next.handle(token_request);
+  }
+}
