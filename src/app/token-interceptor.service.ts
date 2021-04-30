138d072d94e9af99a25c8b0ac523b853503cff81
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-30 16:57:18 +0530

    added refresh token handler

--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpHandler, HttpInterceptor } from "@angular/common/http";
+import { catchError, retry } from 'rxjs/operators';
+import { ApiServiceService } from './api-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor{
 
-  constructor() { }
-  getToken(){
-    return localStorage.getItem('token');
+  constructor(private api: ApiServiceService) { }
+
+  reAuthenticate(){
+    const user = localStorage.getItem('user')||'';
+    const parsedUser = JSON.parse(user);
+    this.api.refreshToken(parsedUser).subscribe(data=>{
+      const response:any = data;
+      const token = response.token;
+      localStorage.setItem('token', token);
+    }, (error)=>{
+      console.log(error)
+    });
   }
   intercept(req: any, next: HttpHandler){
     const token_request = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${this.getToken()}`
+        Authorization: `Bearer ${localStorage.getItem('token')}`
       }
     })
-    return next.handle(token_request);
+    return next.handle(token_request).pipe(catchError(error=>{
+        if(error.status===403){
+          this.reAuthenticate();
+          retry(1)
+        }
+        return next.handle(token_request)
+      })
+    );
   }
 }
