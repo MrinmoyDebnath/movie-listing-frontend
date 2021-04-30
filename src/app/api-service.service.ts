3ab25046a3e6475882a3ef035901381d9055c696
Mrinmoy Debnath <mrinmoy.debnath@mountblue.tech>
2021-04-30 16:55:45 +0530

    added pipe for error handling

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,70 +10,70 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 export class ApiServiceService {
 
   constructor(private http: HttpClient) { }
-  apiUrl = 'http://localhost:3000';
-  
-  getToken():string{
-    return localStorage.getItem('token') || '';
-  }
 
-  // header = new HttpHeaders({
-  //   'Content-Type':'application/json; charset=utf-8',
-  //   'authorization': this.getToken()  
-  // })
+  handleError(error: any){
+    return throwError(error)
+  } 
   getMovies(page: any, limit: any){
-    const url = `${this.apiUrl}/movies?page=${page}&limit=${limit}`;
-    return this.http.get(url);
+    const url = `${environment.API_URL}/movies?page=${page}&limit=${limit}`;
+    return this.http.get(url).pipe(catchError(this.handleError))
   }
   getMovieByName(name: any){
-    const url = `${this.apiUrl}/movies/${name}`;
-    return this.http.get(url);
+    const url = `${environment.API_URL}/movies/${name}`;
+    return this.http.get(url).pipe(catchError(this.handleError))
   }
   getMoviesOfActor(name: any, page: any, limit: any){
-    const url = `${this.apiUrl}/movies/actor/${name}?page=${page}&limit=${limit}`;
-    return this.http.get(url);
+    const url = `${environment.API_URL}/movies/actor/${name}?page=${page}&limit=${limit}`;
+    return this.http.get(url).pipe(catchError(this.handleError))
   }
   getMoviesOfProducer(name: any, page: any, limit: any){
-    const url = `${this.apiUrl}/movies/producer/${name}?page=${page}&limit=${limit}`;
-    return this.http.get(url);
+    const url = `${environment.API_URL}/movies/producer/${name}?page=${page}&limit=${limit}`;
+    return this.http.get(url).pipe(catchError(this.handleError))
   }
   getActorByName(name: any){
-    const url = `${this.apiUrl}/actors/${name}`;
-    return this.http.get(url);  
+    const url = `${environment.API_URL}/actors/${name}`;
+    return this.http.get(url)
+    
+    .pipe(catchError(this.handleError))  
   }
   getActors(page: any, limit: any){
-    const url = `${this.apiUrl}/actors?page=${page}&limit=${limit}`;
-    return this.http.get(url);
+    const url = `${environment.API_URL}/actors?page=${page}&limit=${limit}`;
+    return this.http.get(url).pipe(catchError(this.handleError))
   }
   getProducerByName(name: any){
-    const url = `${this.apiUrl}/producers/${name}`;
-    return this.http.get(url);
+    const url = `${environment.API_URL}/producers/${name}`;
+    return this.http.get(url).pipe(catchError(this.handleError))
   }
   getProducers(page: any, limit: any){
-    const url = `${this.apiUrl}/producers?page=${page}&limit=${limit}`;
-    return this.http.get(url);
+    const url = `${environment.API_URL}/producers?page=${page}&limit=${limit}`;
+    return this.http.get(url).pipe(catchError(this.handleError))
   }
   editMovie(data: any){
-    const url = `${this.apiUrl}/movies`;
-    return this.http.put(url, data);
+    const url = `${environment.API_URL}/movies`;
+    return this.http.put(url, data).pipe(catchError(this.handleError))
   }
   createActor(data: any){
-    const url = `${this.apiUrl}/actors`;
-    return this.http.post(url, data);
+    const url = `${environment.API_URL}/actors`;
+    return this.http.post(url, data).pipe(catchError(this.handleError))
   }
   createMovie(data: any){
-    const url = `${this.apiUrl}/movies`;
-    return this.http.post(url, data);
+    const url = `${environment.API_URL}/movies`;
+    return this.http.post(url, data).pipe(catchError(this.handleError))
   }
   createProducer(data: any){
-    const url = `${this.apiUrl}/producers`;
-    return this.http.post(url, data);
+    const url = `${environment.API_URL}/producers`;
+    return this.http.post(url, data).pipe(catchError(this.handleError))
   }
   createAccount(data: any){
-    const url = `${this.apiUrl}/user/signup`;
-    return this.http.post(url, data);
+    const url = `${environment.API_URL}/user/signup`;
+    return this.http.post(url, data).pipe(catchError(this.handleError))
   }
   loginAccount(data: any){
-    const url = `${this.apiUrl}/user/login`;
-    return this.http.post(url, data);
+    const url = `${environment.API_URL}/user/login`;
+    return this.http.post(url, data).pipe(catchError(this.handleError))
+  }
+  refreshToken(data: any){
+    const url = `${environment.API_URL}/user/login`;
+    return this.http.post(url, data).pipe(catchError(this.handleError))
   }
 }
